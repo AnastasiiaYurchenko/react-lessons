48264c12d29c8c37a3bd31e5043c7891e8ff9ad7
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-04-24 01:07:21 +0300

    delete schema

--- src/components/RecipeForm/RecipeForm.js
+++ src/components/RecipeForm/RecipeForm.js
@@ -2,17 +2,7 @@ import { FormField, Form } from './RecipeForm.styled';
 import { Formik, Field } from 'formik';
 import * as Yup from 'yup';
 
-const SignupSchema = Yup.object().shape({
-  firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
-    .required('Required'),
-  email: Yup.string().email('Invalid email').required('Required'),
-});
+
 
 const RecipeSchema = Yup.object().shape({
   name: Yup.string().required('Required'),
